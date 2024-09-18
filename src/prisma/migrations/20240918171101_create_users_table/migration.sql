1aa3316bd9dc7ff316b6873517870a38ee36f44f
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2024-09-18 22:42:04 +0530

    added models

---
+++ src/prisma/migrations/20240918171101_create_users_table/migration.sql
@@ -0,0 +1,46 @@
+-- CreateEnum
+CREATE TYPE "RoleType" AS ENUM ('ADMIN', 'CUSTOMER', 'FLIGHT_COMPANY');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "name" "RoleType" NOT NULL DEFAULT 'CUSTOMER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserRole" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "roleId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserRole_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserRole_userId_roleId_key" ON "UserRole"("userId", "roleId");
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/repositories/crud-repositrory.js
@@ -0,0 +1,63 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { StatusCodes } = require('http-status-codes');
+const AppError = require('../utils/errors/app-error');
+
+
+class CrudRepository {
+    constructor(model) {
+        this.model = model; // Referencing the Prisma model dynamically
+    }
+
+    async create(data) {
+        const response = await this.model.create({
+            data
+        });
+        return response;
+    }
+
+    async destroy(id) {
+        const response = await this.model.delete({
+            where: {
+                id: id
+            }
+        });
+        if (!response) {
+            throw new AppError('Not able to find the resource', StatusCodes.NOT_FOUND);
+        }
+        return response;
+    }
+
+    async get(id) {
+        const response = await this.model.findUnique({
+            where: {
+                id: id
+            }
+        });
+        if (!response) {
+            throw new AppError('Not able to find the resource', StatusCodes.NOT_FOUND);
+        }
+        return response;
+    }
+
+    async getAll() {
+        const response = await this.model.findMany();
+        return response;
+
+    }
+
+    async update(id, data) {
+        const response = await this.model.update({
+            where: {
+                id: id
+            },
+            data: data
+        });
+        if (!response) {
+            throw new AppError('Not able to find the resource', StatusCodes.NOT_FOUND);
+        }
+        return response;
+    }
+}
+
+module.exports = CrudRepository;
